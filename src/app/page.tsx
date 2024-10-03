42fa38f411cab8b8592f5cd7d83cb05df2871d14
DevYe <yesison714@gmail.com>
2024-10-03 00:13:33 -0500

    primeros pasos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,21 +6,21 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
+          src="/imgs/starman_750x750.png"
           alt="Next.js logo"
-          width={180}
-          height={38}
+          width={500}
+          height={500}
           priority
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing by Yesid Banguera{" "}
+            Este sera la siguiente ecoomerce para desarrollar{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
+              src/app/page.tsx 
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>Comming soon</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
